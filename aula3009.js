88d1d39b413513f5f56cdffa4a2679c81f4ba239
FabricioFodi <fabricio_fodi@estudante.sc.senai.br>
2024-10-01 16:54:41 -0300

    update 01/10

--- aula3009.js
+++ aula3009.js
@@ -34,47 +34,19 @@ document.getElementById('cadastrarTarefas').addEventListener('click', () => {
         titulo: 'Estudar JS',
         decricao: 'Descrição da tarefa'
     };
-
-    const successAlert = document.getElementById('successAlert');
-    const errorAlert = document.getElementById('errorAlert');
-
-    // Faz a requisição POST para cadastrar a tarefa
     fetch('https://crudcrud.com/api/a94488c22fb64d13bbab01567f215649/todos', {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify(tarefa)
-    })
-    .then((Response) => {
-        if (!Response.ok) {
-            throw new Error('Falha ao cadastrar tarefa');
-        }
+    }).then((Response) => {
+        console.log(Response);
         return Response.json();
-    })
-    .then((data) => {
-        console.log('Tarefa cadastrada com sucesso:', data);
-        
-        // Exibe o alerta de sucesso
-        successAlert.classList.remove('hidden');
-        errorAlert.classList.add('hidden');
-        
-        // Esconde o alerta automaticamente após 3 segundos
-        setTimeout(() => {
-            successAlert.classList.add('hidden');
-        }, 3000);
-    })
-    .catch((error) => {
-        console.error('Erro ao cadastrar tarefa:', error);
-
-        // Exibe o alerta de erro
-        errorAlert.classList.remove('hidden');
-        successAlert.classList.add('hidden');
-        
-        // Esconde o alerta automaticamente após 3 segundos
-        setTimeout(() => {
-            errorAlert.classList.add('hidden');
-        }, 3000);
+    }).then((Response) => {
+        console.log(Response);
+    }).catch((error) => {
+        console.error(error);
     });
 });
 
