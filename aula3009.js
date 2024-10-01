eed61402edc39025eb9cc18e4df8c81d9a79a22d
FabricioFodi <fabricio_fodi@estudante.sc.senai.br>
2024-10-01 16:04:57 -0300

    update 01/10

--- aula3009.js
+++ aula3009.js
@@ -2,16 +2,16 @@ const cadastrar = document.getElementById('cadastrarTarefas');
 const consultar = document.getElementById('consultarTarefas');
 const atualizar = document.getElementById('atualizarTarefas');
 const deletar = document.getElementById('deletarTarefas');
-
+const logOriginal = console.log;
 
 atualizar.addEventListener('click', () => {
     const tarefa = {
-        _id: '66fb2c6dfe837603e816d9e1',
+        _id: '66fbdf4cfe837603e816db59',
         id: 1,
         titulo: 'Estudar JS dois',
         decricao: 'Descrição da segunda tarefa'
     };
-    fetch('https://crudcrud.com/api/a94488c22fb64d13bbab01567f215649/todos/66fb2c6dfe837603e816d9e1', {
+    fetch('https://crudcrud.com/api/a94488c22fb64d13bbab01567f215649/todos/66fbe284fe837603e816db65', {
         method: 'PUT',
         headers: {
             'content-type': 'application/json'
@@ -27,27 +27,57 @@ atualizar.addEventListener('click', () => {
     });
 });
 
-cadastrar.addEventListener('click', () => {
+
+document.getElementById('cadastrarTarefas').addEventListener('click', () => {
     const tarefa = {
         id: 1,
         titulo: 'Estudar JS',
         decricao: 'Descrição da tarefa'
-    }
-    fetch('https://crudcrud.com/api/a94488c22fb64d13bbab01567f215649/todos',{
+    };
+
+    const successAlert = document.getElementById('successAlert');
+    const errorAlert = document.getElementById('errorAlert');
+
+    // Faz a requisição POST para cadastrar a tarefa
+    fetch('https://crudcrud.com/api/a94488c22fb64d13bbab01567f215649/todos', {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify(tarefa)
-    }).then((Response) => {
-        console.log(Response);
+    })
+    .then((Response) => {
+        if (!Response.ok) {
+            throw new Error('Falha ao cadastrar tarefa');
+        }
         return Response.json();
-    }).then((Response) => {
-        console.log(Response);
-}).catch((error) => {
-    console.log(error);
+    })
+    .then((data) => {
+        console.log('Tarefa cadastrada com sucesso:', data);
+        
+        // Exibe o alerta de sucesso
+        successAlert.classList.remove('hidden');
+        errorAlert.classList.add('hidden');
+        
+        // Esconde o alerta automaticamente após 3 segundos
+        setTimeout(() => {
+            successAlert.classList.add('hidden');
+        }, 3000);
+    })
+    .catch((error) => {
+        console.error('Erro ao cadastrar tarefa:', error);
+
+        // Exibe o alerta de erro
+        errorAlert.classList.remove('hidden');
+        successAlert.classList.add('hidden');
+        
+        // Esconde o alerta automaticamente após 3 segundos
+        setTimeout(() => {
+            errorAlert.classList.add('hidden');
+        }, 3000);
+    });
 });
-});
+
 
 consultar.addEventListener('click', () => {
     fetch('https://crudcrud.com/api/a94488c22fb64d13bbab01567f215649/todos', 
@@ -60,4 +90,26 @@ consultar.addEventListener('click', () => {
     }).then((Response) => {
         console.log(Response);
     })
+});
+
+deletar.addEventListener('click', () => {
+    const tarefa = {
+        _id: '66fb2c6dfe837603e816d9e1',
+        id: 1,
+        titulo: 'Estudar JS',
+        decricao: 'Descrição da tarefa'
+    };
+    fetch('https://crudcrud.com/api/a94488c22fb64d13bbab01567f215649/todos/66fbe284fe837603e816db65', {
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(tarefa)
+    }).then((Response) => {
+        console.log(Response);
+    }).then((Response) => {
+        console.log(Response);
+    }).catch((error) => {
+        console.log(error);
+    });
 });
