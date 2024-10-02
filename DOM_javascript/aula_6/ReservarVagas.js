1d663111c7f082c6959b41d3407462bf953008a0
FabricioFodi <fabricio_fodi@estudante.sc.senai.br>
2024-10-02 17:06:49 -0300

    update 02/10

---
+++ DOM_javascript/aula_6/ReservarVagas.js
@@ -0,0 +1,5 @@
+const placaCarro = document.getElementById('placaVeiculo');
+const numeroApt = document.getElementById('aptNum');
+const blocoApt = document.getElementById('blocoApt');
+const numeroVaga = document.getElementById('numVaga');
+
